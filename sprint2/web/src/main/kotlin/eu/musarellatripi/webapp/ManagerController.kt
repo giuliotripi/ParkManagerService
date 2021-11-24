80ee746a1073fcb12e8c680ee5f1ea644e6a6f92
Giulio Tripi <giulio@tripi.eu>
2021-11-24 17:34:10 +0100

    updated GUI in sprint 2

--- sprint2/web/src/main/kotlin/eu/musarellatripi/webapp/ManagerController.kt
+++ sprint2/web/src/main/kotlin/eu/musarellatripi/webapp/ManagerController.kt
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.ResponseBody
 class ManagerController {
     @GetMapping("/manager")
     fun homePage(model: Model): String {
+        model.addAttribute("TMAX", Values.TMAX)
         return "manager"
     }
 
