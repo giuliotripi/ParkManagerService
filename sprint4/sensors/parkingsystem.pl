57fcca014386d9b9da393d102f4efb18ee42780f
Giulio Tripi <giulio@tripi.eu>
2021-12-06 22:38:50 +0100

    service in sensors is variable

--- sprint4/sensors/parkingsystem.pl
+++ sprint4/sensors/parkingsystem.pl
@@ -1,8 +1,8 @@
 %====================================================================================
-% parkingsystem description   
+% parkingsystem description
 %====================================================================================
 context(ctxsensor, "127.0.0.1",  "TCP", "8054").
-context(ctxservice, "192.168.1.130",  "TCP", "8050").
+context(ctxservice, "service.local",  "TCP", "8050").
  qactor( thermometer, ctxsensor, "it.unibo.thermometer.Thermometer").
   qactor( sonar, ctxsensor, "it.unibo.sonar.Sonar").
   qactor( fan, ctxsensor, "it.unibo.fan.Fan").
